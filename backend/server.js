3fa0c70676e83270a08275c027ea82cb6cc01b13
ParamiDinethma <paramirana2004@gmail.com>
2026-04-26 06:23:06 +0530

    add app listen

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
-const itemsRouter = require('./routes/items');
-app.use('/api/items', itemsRouter);
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log("Error connecting to MongoDB:", err));
 
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+  // Routes
+const itemsRouter = require('./routes/items');
+app.use('/api/items', itemsRouter);
 
